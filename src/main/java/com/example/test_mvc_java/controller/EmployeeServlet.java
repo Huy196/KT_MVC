1b17f9a2a1e5bd01556a30edf241618091ba41ba
doquochuy <doquochuyy66@gmail.com>
2024-12-11 08:33:11 +0700

    update_employee

--- src/main/java/com/example/test_mvc_java/controller/EmployeeServlet.java
+++ src/main/java/com/example/test_mvc_java/controller/EmployeeServlet.java
@@ -1,18 +1,16 @@
 package com.example.test_mvc_java.controller;
-
 import com.example.test_mvc_java.model.Employee;
 import com.example.test_mvc_java.service.EmployeeService;
 import com.example.test_mvc_java.service.EmployeeServicelmpl;
-
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.util.List;
-import java.util.Random;
 
 @WebServlet(name = "EmployeeServlet", value = "/employee")
 public class EmployeeServlet extends HttpServlet {
@@ -40,42 +38,18 @@ public class EmployeeServlet extends HttpServlet {
         }
     }
 
-//    private void updateEmployeeForm(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//        RequestDispatcher dispatcher = req.getRequestDispatcher("/employee/UpdateEmployee.jsp");
-//        dispatcher.forward(req, resp);
-//    }
 
     private void updateEmployee(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        try {
-            int id = Integer.parseInt(req.getParameter("id"));
-            String name = req.getParameter("name");
-            int age = Integer.parseInt(req.getParameter("age"));
-            String jobPosition = req.getParameter("jobPosition");
-            String departments = req.getParameter("departments");
-            double salary = Double.parseDouble(req.getParameter("salary"));
+        int id = Integer.parseInt(req.getParameter("id"));
 
-            Employee employee = employeeService.searchById(id);
+        Employee employee = employeeService.searchById(id);
+        req.setAttribute("employee", employee);
+        req.getSession().setAttribute("id",id);
 
-            if (employee != null) {
-                employee.setName(name);
-                employee.setAge(age);
-                employee.setJobPosition(jobPosition);
-                employee.setDepartments(departments);
-                employee.setSalary(salary);
+        RequestDispatcher dispatcher = req.getRequestDispatcher("/employee/UpdateEmployee.jsp");
+        dispatcher.forward(req, resp);
 
-                employeeService.update(id, employee);
 
-                req.setAttribute("employee", employee);
-                RequestDispatcher dispatcher = req.getRequestDispatcher("/employee/UpdateEmployee.jsp");
-                dispatcher.forward(req, resp);
-            } else {
-                req.setAttribute("error", "Không tìm thấy nhân viên với ID: " + id);
-                req.getRequestDispatcher("/update.jsp").forward(req, resp);
-            }
-        } catch (NumberFormatException e) {
-            req.setAttribute("error", "Dữ liệu đầu vào không hợp lệ.");
-            req.getRequestDispatcher("/update.jsp").forward(req, resp);
-        }
     }
 
     private void showEmployeeForm(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -102,10 +76,37 @@ public class EmployeeServlet extends HttpServlet {
             case "add":
                 addEmployee(req, resp);
             case "update":
-//                updateEmployee(req, resp);
+                updateEmployeeToData(req, resp);
             default:
                 break;
         }
+    }
+
+    private void updateEmployeeToData(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
+        HttpSession session = req.getSession();
+        Object ids = session.getAttribute("id");
+
+        int id = Integer.parseInt(ids.toString());
+
+        Employee employee = employeeService.searchById(id);
+
+        String name = req.getParameter("name");
+        int age = Integer.parseInt(req.getParameter("age"));
+        String jobPosition = req.getParameter("jobPosition");
+        String departments = req.getParameter("departments");
+        double salary = Double.parseDouble(req.getParameter("salary"));
+
+        employee.setName(name);
+        employee.setAge(age);
+        employee.setJobPosition(jobPosition);
+        employee.setDepartments(departments);
+        employee.setSalary(salary);
+
+        employeeService.update(id, employee);
+
+        session.removeAttribute("id");
+
+        resp.sendRedirect(req.getContextPath() + "/employee");
     }
 
     private void deleteEmployee(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -121,7 +122,6 @@ public class EmployeeServlet extends HttpServlet {
         String jobPosition = req.getParameter("jobPosition");
         String departments = req.getParameter("departments");
         double salary = Double.parseDouble(req.getParameter("salary"));
-        System.out.println(name + age);
 
         Employee employee = new Employee(id, name, age, jobPosition, departments, salary);
 
